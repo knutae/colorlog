a8072d0591c2794dd7828d1e942c20930a4c2904
Knut Arild Erstad <knutae@gmail.com>
2013-06-09 14:13:09 +0200

    Added helper class, and compile a single rexeg for matching each line. Improves speed.

--- colorlog.py
+++ colorlog.py
@@ -17,27 +17,39 @@ def colorize_line(line, fg_color, bright):
     brightness = '1' if bright else '0'
     return prefix + brightness + ';' + str(fg_color) + suffix + line + prefix + suffix
 
-def colorize_stream(rules, input, output):
-    for line in input:
-        for regex, col, bright in rules:
-            if regex.search(line) is not None:
-                line = colorize_line(line, col, bright)
-                break
-        output.write(line)
+class RuleProcessor:
+    def __init__(self, rules):
+        self.rulemap = dict()
+        for keyword, color, bright in rules:
+            self.rulemap[keyword] = (color, bright)
+        self.regex = re.compile('(' + '|'.join(self.rulemap.keys()) + ')')
+    
+    def process_line(self, line):
+        m = self.regex.search(line)
+        if m is None:
+            return line
+        keyword = m.group()
+        color, bright = self.rulemap[keyword]
+        return colorize_line(line, color, bright)
+    
+    def process_stream(self, input, output):
+        for line in input:
+            line = self.process_line(line)
+            output.write(line)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-L', '--less', action='store_true', help="view output in less (pipe to 'less -R')")
     parser.add_argument('files', nargs='*', help='files to read (if empty, read standard input)')
     args = parser.parse_args()
-    rules = [
-        (re.compile('TRACE'), GRAY, False),
-        (re.compile('INFO'), GREEN, True),
-        (re.compile('WARN'), YELLOW, True),
-        (re.compile('DEBUG'), CYAN, True),
-        (re.compile('ERROR'), RED, True),
-        (re.compile('FATAL'), MAGENTA, True),
-    ]
+    processor = RuleProcessor([
+        ('TRACE', GRAY, False),
+        ('INFO', GREEN, True),
+        ('WARN', YELLOW, True),
+        ('DEBUG', CYAN, True),
+        ('ERROR', RED, True),
+        ('FATAL', MAGENTA, True),
+    ])
     process = None
     if args.less:
         process = subprocess.Popen(['less', '-R'], stdin=subprocess.PIPE)
@@ -48,9 +60,9 @@ if __name__ == '__main__':
         if len(args.files) > 0:
             for fname in args.files:
                 with open(fname) as f:
-                    colorize_stream(rules, f, output)
+                    processor.process_stream(f, output)
         else:
-            colorize_stream(rules, sys.stdin, output)
+            processor.process_stream(sys.stdin, output)
     except IOError as e:
         if e.errno == errno.EPIPE:
             # ignore broken pipe errors
